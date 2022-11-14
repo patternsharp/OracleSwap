a3d8f87a6a990ccd3a4a16103d0436f333cb22c9
superGru625 <scarabchain@gmail.com>
2022-11-14 13:43:02 -0800

    update rpc

--- src/modals/NetworkModal/index.tsx
+++ src/modals/NetworkModal/index.tsx
@@ -204,7 +204,7 @@ export const SUPPORTED_NETWORKS: {
       symbol: 'SGB',
       decimals: 18,
     },
-    rpcUrls: ['https://rpc.sgbftso.com/http'],
+    rpcUrls: ['https://sgb.ftso.com.au/ext/bc/C/rpc'],
     blockExplorerUrls: ['https://songbird-explorer.flare.network'],
   },
 }
