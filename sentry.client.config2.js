c0dbdcdacbb611f6a78a042763aed4698d8fcd6a
superGru625 <scarabchain@gmail.com>
2023-01-09 05:07:39 +0200

    update the vercel deploy issue

---
+++ sentry.client.config2.js
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/nextjs";
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN || "https://98295dd2363c40bf95627f05985131cd@o4504449948516352.ingest.sentry.io/4504450023555072",
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
