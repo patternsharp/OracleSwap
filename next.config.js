30d14c93f7abc32b6440a7d667cd27e29d1ead8b
superGru625 <scarabchain@gmail.com>
2023-01-26 00:22:57 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -146,7 +146,24 @@ const nextConfig = {
   publicRuntimeConfig: {
     breakpoints: screens,
   },
+
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+
 }
+
 
 const SentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
